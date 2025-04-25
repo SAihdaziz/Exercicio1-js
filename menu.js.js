37a7fa72ed27d5e5bf963d4d29149eb35a16f94b
saido ture <saidoture82@gmail.com>
2025-04-25 07:25:21 -0300

    Projeto Exercicio1.js - coleta e comparação de dados

---
+++ menu.js.js
@@ -0,0 +1,12 @@
+// Primeira janelinha: nome
+let nome = prompt("Digite seu nome:");
+console.log("Nome:", nome);
+
+// Segunda janelinha: idade
+let idade = prompt("Digite sua idade:");
+console.log("Idade:", idade);
+
+// Terceira janelinha: profissão
+let profissao = prompt("Digite sua profissão:");
+console.log("Profissão:", profissao);
+
