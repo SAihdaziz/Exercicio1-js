7214f9eb12ad9e1a16f31df2d9a92134aca4d425
saido ture <saidoture82@gmail.com>
2025-04-28 06:32:20 -0300

    Fiz alteração no projeto, agora é para cadastro de carros tipos modelos e marcas de carros.

--- menu.js.js
+++ menu.js.js
@@ -1,12 +1,24 @@
-// Primeira janelinha: nome
-let nome = prompt("Digite seu nome:");
-console.log("Nome:", nome);
+// Primeira janelinha: Tipo do carro
+let tipo = prompt("Digite o tipo do carro (ex: Sedan, SUV, Hatch):");
+console.log("Tipo:", tipo);
 
-// Segunda janelinha: idade
-let idade = prompt("Digite sua idade:");
-console.log("Idade:", idade);
+// Segunda janelinha: Modelo do carro
+let modelo = prompt("Digite o modelo do carro:");
+console.log("Modelo:", modelo);
 
-// Terceira janelinha: profissão
-let profissao = prompt("Digite sua profissão:");
-console.log("Profissão:", profissao);
+// Terceira janelinha: Placa do carro
+let placa = prompt("Digite a placa do carro:");
+console.log("Placa:", placa);
+
+// Quarta janelinha: Ano do carro
+let ano = prompt("Digite o ano do carro:");
+console.log("Ano:", ano);
+
+// Quinta janelinha: Motor do carro
+let motor = prompt("Digite o motor do carro (ex: 1.0, 1.5, 2.0):");
+console.log("Motor:", motor);
+
+// Sexta janelinha: Combustível do carro
+let combustivel = prompt("Digite o tipo de combustível (Gasolina, Álcool, Flex, Diesel):");
+console.log("Combustível:", combustivel);
 
